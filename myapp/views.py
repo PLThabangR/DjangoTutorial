bbd3223b6f278121919f45de8a6fc78c1ed0a8d5
PLThabangR <rakgoropthabang@gmail.com>
2024-06-08 15:36:25 +0200

    Comments added on the view

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse 
+
+#The view function reads the path, query, and body parameters included in the client's request If required, 
+# it uses this data to interact with the models to perform CRUD operations.
 def home(request): 
     return HttpResponse("Hello, world. This is the.") 
 # Create your views here.
